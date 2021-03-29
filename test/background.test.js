f7ac842851f6958fc29d3dd9c697000c6f46de97
unknown <motercode@gmail.com>
2021-03-29 11:15:49 +0200

    added some test as example of mocking with sinon

--- test/background.test.js
+++ test/background.test.js
@@ -2,6 +2,23 @@ const chrome =  require("sinon-chrome/extensions");
 global.chrome = chrome;
 const background = require('src/background.js')
 
+function PageStateMatcher(datos) {
+  this.pageUrl = datos.pageUrl.urlMatches;
+}
+
+
+function ShowPageAction(data) {
+    this.data = data;
+}
+
+chrome.declarativeContent.PageStateMatcher = PageStateMatcher;
+chrome.declarativeContent.ShowPageAction = ShowPageAction;
+chrome.declarativeContent.onPageChanged.rules = [];
+chrome.declarativeContent.onPageChanged.removeRules = (rule, callback) => {callback();};
+chrome.declarativeContent.onPageChanged.addRules = (arrayrules) => {
+      chrome.declarativeContent.onPageChanged.rules=arrayrules;
+};
+
 
 describe("background.js ", () => {
   beforeAll(() => {
@@ -13,7 +30,7 @@ describe("background.js ", () => {
   });
 
 
-  describe("schema test for api chrome calls ", () => {
+  describe("it hooks the popup-content.html to chrome.declarativeContent when called onInstalled ", () => {
     it ('had been not been called runtime.oninstalled.addlistener without call the function' , (done)=>{
       expect (chrome.runtime.onInstalled.addListener.calledOnce).toBe(false);
       done();
@@ -24,8 +41,25 @@ describe("background.js ", () => {
       done();
     });
 
+    it ('adds a new rule with pageurl and actions as required when onInstalled dispatch', (done) =>{
+      var pageUrlTarget = '.';
+      var actionsTarget = new chrome.declarativeContent.ShowPageAction();
 
+      background.addthehookoninstalled();
+
+      expect(chrome.declarativeContent.onPageChanged.rules.length).toBe(0);
+
+      chrome.runtime.onInstalled.dispatch();
+
+      expect(chrome.declarativeContent.onPageChanged.rules.length).toBe(1);
+
+      expect(chrome.declarativeContent.onPageChanged.rules[0].conditions[0].pageUrl).toEqual(pageUrlTarget);
+      expect(chrome.declarativeContent.onPageChanged.rules[0].actions[0]).toEqual(actionsTarget);
+
+      done();
+    });
   });
+
   afterAll(() => {
     chrome.flush();
     delete global.chrome;
